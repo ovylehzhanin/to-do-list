9d7838362ae0f190e8f2bd640feb5be50300f3d8
Alexey Vylegzhanin <a.a.vylegzhanin@gmail.com>
2017-08-02 22:19:26 +0300

    test addItem

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,55 @@ const DOM = function() {
 };
 
 const App = function (page) {
-  this.name = 'test';
+  let data = {
+    storageName: 'taskList',
+    taskList: null
+  };
+
+  let appStorage = function(key, value) {
+    if (!value) {
+      return localStorage.getItem(key);
+    } else {
+      localStorage.setItem(key, value);
+    }
+  };
+
+  let addItem = function() {
+    let date = new Date,
+      year = date.getFullYear(),
+      month = date.getMonth(),
+      day = date.getDay(),
+      hours = date.getHours(),
+      minutes = date.getMinutes(),
+      seconds = date.getSeconds();
+    
+    let item = {
+        id: date.valueOf(),
+        status: 'new',
+        content: page.textInput.value
+      };
+
+    let template = `
+      <li class="task task--checked">
+        <div class="task__inner">
+          <i class="task__action task__check fa fa-circle-o"></i>
+          <div class="content">Inner text</div>
+          <i class="task__action task__remove fa fa-trash"></i>
+        </div>
+      </li>
+    `;
+
+    page.taskList.innerHTML += template;
+  };
+
+  let bindEvents = function() {
+    let _app = this;
+
+    page.addItemButton.addEventListener('click', addItem, false);
+
+  };
 
   this.init = function() {
-    console.log(this.name);
+    bindEvents();
   }
 };
